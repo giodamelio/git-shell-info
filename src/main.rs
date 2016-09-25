801e78d529821b84dc1e8ade5bc1bd54d999b776
Gio d'Amelio <giodamelio@gmail.com>
2016-09-24 18:27:29 -0700

    Install git2 and show the status of files in the working tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate git2;
+
+use std::env;
+
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+    let path = match env::current_dir() {
+        Ok(path) => path,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    let repo = match Repository::open(path) {
+        Ok(repo) => repo,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    for status_entry in repo.statuses(None).unwrap().iter() {
+        let status = status_entry.status();
+        let path = status_entry.path().unwrap();
+        let status_text = match status {
+            s if s.contains(git2::STATUS_WT_MODIFIED) => "modified",
+            s if s.contains(git2::STATUS_WT_DELETED) => "deleted",
+            s if s.contains(git2::STATUS_WT_RENAMED) => "renamed",
+            s if s.contains(git2::STATUS_WT_NEW) => "new",
+            s if s.contains(git2::STATUS_WT_TYPECHANGE) => "typechange",
+            _ => continue,
+        };
+        println!("{}: {}", status_text, path);
+    }
 }
