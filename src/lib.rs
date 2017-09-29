5fba7baecc9bff5d198ef175a6a7832d7be8a214
Gio d'Amelio <giodamelio@gmail.com>
2017-09-29 13:28:47 -0700

    Open a repo with libgit2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+extern crate git2;
+
+use git2::Repository;
+
+pub fn open_repo() {
+    let repo = match Repository::open(env!("CARGO_MANIFEST_DIR")) {
+        Ok(repo) => repo,
+        Err(e) => panic!("falied to open repo: {}", e),
+    };
+    println!("Opened repo: {:#?}", repo.path());
+}
+
 #[cfg(test)]
 mod tests {
+    use super::open_repo;
+
     #[test]
-    fn it_works() {
+    fn opens_a_repo() {
+        open_repo();
         assert_eq!(2 + 2, 4);
     }
 }
